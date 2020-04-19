76fb5ba9869057ed1ffa24188feda3b4947df2fa
MiVladie <mivladie@github.com>
2020-04-19 17:01:05 +0100

    Updates

---
+++ precache-manifest.4504d59dda1fe3d8f67473058355b8b6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb753b51705850b5c063864ec846af5",
+    "url": "/shergilldesign/index.html"
+  },
+  {
+    "revision": "fd766fe69c5533c2eb7b",
+    "url": "/shergilldesign/static/css/main.499e4877.chunk.css"
+  },
+  {
+    "revision": "6b0281fe2e7e3af5ab0f",
+    "url": "/shergilldesign/static/js/2.226f6fef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shergilldesign/static/js/2.226f6fef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd766fe69c5533c2eb7b",
+    "url": "/shergilldesign/static/js/main.626cbaf5.chunk.js"
+  },
+  {
+    "revision": "e7152f03bcb2a91d0bee",
+    "url": "/shergilldesign/static/js/runtime-main.815bfddf.js"
+  },
+  {
+    "revision": "94753d970d4207c16774fa6252701cf0",
+    "url": "/shergilldesign/static/media/logo.94753d97.png"
+  },
+  {
+    "revision": "e3156131a36729eed3de554d8c4f39f9",
+    "url": "/shergilldesign/static/media/photo_1.e3156131.jpg"
+  },
+  {
+    "revision": "c48d529819ee992a7952b3588c9edad5",
+    "url": "/shergilldesign/static/media/photo_2.c48d5298.jpg"
+  },
+  {
+    "revision": "1ca12a73b61e02e6fad4e13ffc9f3fcd",
+    "url": "/shergilldesign/static/media/template_01.1ca12a73.jpg"
+  },
+  {
+    "revision": "d40904636c63dd6018524fdfd22b3ec3",
+    "url": "/shergilldesign/static/media/template_02.d4090463.jpg"
+  },
+  {
+    "revision": "f8a5e8abd0e326d4e312090a3cbc00c9",
+    "url": "/shergilldesign/static/media/template_03.f8a5e8ab.jpg"
+  },
+  {
+    "revision": "76f4f000fdddb91d196cc20178cf8c7c",
+    "url": "/shergilldesign/static/media/template_04.76f4f000.jpg"
+  },
+  {
+    "revision": "b08d37ac51c312466db5d297e374436a",
+    "url": "/shergilldesign/static/media/template_05.b08d37ac.jpg"
+  },
+  {
+    "revision": "7c89ff2e81b206b99c87386020d5412b",
+    "url": "/shergilldesign/static/media/template_07.7c89ff2e.jpg"
+  }
+]);
